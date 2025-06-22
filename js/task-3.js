07b71dccd95aeaa5aff93b8e66f2eeffa6ea8f08
igor <igordanko2222@gmail.com>
2025-06-22 22:00:33 +0300

    add homework

--- js/task-3.js
+++ js/task-3.js
@@ -1,18 +1,24 @@
 console.log("3 ------------------------------------------");
 
-function filterArray(numbers, value) {
-  let array = [];
-  for (let i = 0; numbers.length > i; i++) {
-    if (value < numbers[i]) {
-      array.push(numbers[i]);
-    }
-  }
-  return array;
-}
+const profile = {
+  username: "Jacob",
+  playTime: 300,
+  changeUsername(newName) {
+    this.username = newName;
+  },
+  updatePlayTime(hours) {
+    this.playTime += hours;
+  },
+  getInfo() {
+    return `${this.username} has ${this.playTime} active hours`;
+  },
+};
 
-console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
-console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
-console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
-console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
-console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+
+profile.changeUsername("Marco");
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
 console.log("------------------------------------------");
